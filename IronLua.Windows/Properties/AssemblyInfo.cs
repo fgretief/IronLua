1c1510778c1c85c8878239a18988c5819a18a9e9
Francois Retief <fgretief@spaceteq.co.za>
2019-02-20 11:10:41 +0200

    Convert IronLua.Windows.csproj to Visual Studio 2017 format

--- IronLua.Windows/Properties/AssemblyInfo.cs
+++ IronLua.Windows/Properties/AssemblyInfo.cs
@@ -1,18 +1,5 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("iluaw")]
-[assembly: AssemblyDescription("IronLua - A Lua runtime for .NET")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("IronLua Team")]
-[assembly: AssemblyProduct("IronLua")]
-[assembly: AssemblyCopyright("IronLua Contributors")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
